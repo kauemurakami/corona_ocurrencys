576deeef03334b58684f774a1a3657e154284ac4
Kauê Murakami <kauetmurakami@gmail.com>
2020-03-21 20:21:13 -0300

    adicionando recurso de ligação ao 136, corrigindo proble,ma de webview para o download do plano de contigencia nacional

--- lib/telas/informacoes.dart
+++ lib/telas/informacoes.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -9,6 +8,14 @@ class Informacoes extends StatefulWidget {
 }
 
 class _InformacoesState extends State<Informacoes> {
+  static const url = "https://portalarquivos2.saude.gov.br/images/pdf/2020/fevereiro/13/plano-contingencia-coronavirus-COVID19.pdf";
+
+  _openSite(String url) async {
+    if(await canLaunch(url)){
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    }else throw 'Navegador não encontarado';
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -52,32 +59,6 @@ class _InformacoesState extends State<Informacoes> {
           Padding(
             padding: EdgeInsets.only(top: 10, bottom: 10),
             child : Text("De uma forma geral, a transmissão viral ocorre apenas enquanto persistirem os sintomas É possível a transmissão viral após a resolução dos sintomas, mas a duração do período de transmissibilidade é desconhecido para o coronavírus. Durante o período de incubação e casos assintomáticos não são contagiosos."),
-          ),
-
-          Text(
-            "Período de transmissibilidade do coronavírus",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.amber
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child : Text("De uma forma geral, a transmissão viral ocorre apenas enquanto persistirem os sintomas É possível a transmissão viral após a resolução dos sintomas, mas a duração do período de transmissibilidade é desconhecido para o coronavírus. Durante o período de incubação e casos assintomáticos não são contagiosos."),
-          ),
-
-          Text(
-            "Fonte de infecção do coronavírus",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.amber
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            child : Text("A maioria dos coronavírus geralmente infectam apenas uma espécie animal ou pelo menos um pequeno número de espécies proximamente relacionadas. Porém, alguns coronavírus, como o SARS-CoV, podem infectar pessoas e animais. O reservatório animal para o coronavírus (COVID-19) ainda é desconhecido. "),
           ),
 
           Text(
@@ -152,10 +133,14 @@ class _InformacoesState extends State<Informacoes> {
                 Text("Uso de umidificador no quarto ou tomar banho quente para auxiliar no alívio da dor de garanta e tosse.\n", style: TextStyle( fontWeight: FontWeight.bold)),
                 Center(
                   child: GestureDetector(
-                    child: Text("PLANO DE CONTIGÊNCIA NACIONAL", style: TextStyle(color: Colors.blue),),
-                    onTap: (){
-                      canLaunch("https://portalarquivos2.saude.gov.br/images/pdf/2020/fevereiro/13/plano-contingencia-coronavirus-COVID19.pdf");
-                    },
+                    child: Text(
+                      "PLANO DE CONTIGÊNCIA NACIONAL \n https://portalarquivos2.saude.gov.br/images/pdf/2020/fevereiro/13/plano-contingencia-coronavirus-COVID19.pdf",
+                      style: TextStyle(
+                          color: Colors.blue,
+                        fontSize: 15,
+                      ),
+                    ),
+                    //onTap: _openSite(url),
                   ),
                 ),
               ],
--- lib/telas/telefones.dart
+++ lib/telas/telefones.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Telefones extends StatefulWidget {
   @override
@@ -7,9 +9,20 @@ class Telefones extends StatefulWidget {
 }
 
 class _TelefonesState extends State<Telefones> {
+
+  _ligar() async {
+    const url = "tel:136";
+    if(await canLaunch(url)){
+      await launch(url);
+    } else {
+      throw 'Aplicativo não encontrado';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Padding(
@@ -26,6 +39,19 @@ class _TelefonesState extends State<Telefones> {
                           fontWeight: FontWeight.bold
                       ),
                   ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                     FloatingActionButton(
+                        onPressed: _ligar,
+                        splashColor: Colors.amber,
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.phone_in_talk,
+                            color: Colors.white
+                        ),
+                    ),
+                  ],
+                ),
               ],
               )
             )
